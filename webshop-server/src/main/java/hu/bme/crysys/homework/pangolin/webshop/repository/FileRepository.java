6a5052a89ea4a39f3b02e505178eacb7449388ed
gurubib <gurubibarni@gmail.com>
2020-10-30 11:22:06 +0100

    Implement UserService.addComment & uuid fixes
    - Implementing UserService.addComment - removing creationDate field form
    AddCommentRequest, since the creationDate must be now
    - Uuid columns now have proper length (36) and they cannot be null

--- webshop-server/src/main/java/hu/bme/crysys/homework/pangolin/webshop/repository/FileRepository.java
+++ webshop-server/src/main/java/hu/bme/crysys/homework/pangolin/webshop/repository/FileRepository.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Repository;
 import hu.bme.crysys.homework.pangolin.webshop.model.File;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
     // TODO - write missing funs
-    List<File> findByFileNameContaining(final String fileNameSearchTerm);
+    List<File> findByFileNameContaining(String fileNameSearchTerm);
+
+    Optional<File> findByUuid(String uuid);
 }
