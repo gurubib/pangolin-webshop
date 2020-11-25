e6841aa48f4fa4c18a597658d54994a6a55eee9e
Gurubi Barni <gurubibarni@gmail.com>
2020-11-25 13:15:02 +0100

    Parser binary integrity check
    - Before the server uses the parser, it always check the integrity of it
    - This is done by firts generating the sha3-256 hash of the actual parser,
    then it is compared to the previously given valid hash of the parser
    - When there is a difference (the parser used by the server has been
    compromised), it won't be used

--- webshop-server/src/main/java/hu/bme/crysys/homework/pangolin/webshop/util/caff/CaffUtils.java
+++ webshop-server/src/main/java/hu/bme/crysys/homework/pangolin/webshop/util/caff/CaffUtils.java
@@ -3,20 +3,28 @@ package hu.bme.crysys.homework.pangolin.webshop.util.caff;
 import hu.bme.crysys.homework.pangolin.webshop.model.File;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.security.crypto.codec.Hex;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Slf4j
 public final class CaffUtils {
 
+    private static final String VALID_PARSER_BINARY_SHA3_256_CHECKSUM = "30ad1ef56d8bebd6bfbe05ba39733b326618c9f294f9c74aee6c24a6d3bd187e";
+    private static final String PARSER_BINARY_LOCATION = "/parser/parser";
+
     private CaffUtils() { }
 
     public static String getFilePreview(final File file) {
-        final Path path = Path.of(file.getLocation(), file.getFileName());
+        final Path caffFilePath = Path.of(file.getLocation(), file.getFileName());
         try {
-            final ProcessBuilder pb = new ProcessBuilder("/parser/parser", path.toString());
+            checkParserIntegrity();
+
+            final ProcessBuilder pb = new ProcessBuilder(PARSER_BINARY_LOCATION, caffFilePath.toString());
             pb.redirectErrorStream(true);
             final Process parser = pb.start();
             parser.waitFor();
@@ -26,6 +34,16 @@ public final class CaffUtils {
             log.error("Error during caff/ciff preview parsing!");
             log.error(e.getMessage());
             return null;
+        }
+    }
+
+    public static void checkParserIntegrity() throws IOException, NoSuchAlgorithmException {
+        final byte[] parserBinary = Files.readAllBytes(Path.of(PARSER_BINARY_LOCATION));
+        final MessageDigest digest = MessageDigest.getInstance("SHA3-256");
+        final String actualParserBinarySha3 = new String(Hex.encode(digest.digest(parserBinary)));
+
+        if (!VALID_PARSER_BINARY_SHA3_256_CHECKSUM.equalsIgnoreCase(actualParserBinarySha3)) {
+            throw new RuntimeException("The integrity of the parser binary has been violated!");
         }
     }
 
